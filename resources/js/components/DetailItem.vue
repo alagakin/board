288641c88aaef9e041423fa8bf1aad0260e30bd1
Александр Агакин <agakinalexandr@gmail.com>
2023-01-29 22:41:28 +0100

    detail page of an item

--- resources/js/components/DetailItem.vue
+++ resources/js/components/DetailItem.vue
@@ -1,5 +1,32 @@
 <template>
-    {{$route.params.id}}
+    <div class="container mt-10">
+        <div class="flex justify-between">
+            <h1 class="text-3xl">Product Name</h1>
+            <div class="mb-4 flex flex-row-reverse">
+                <a href="">
+                    <img src="https://via.placeholder.com/50x50"
+                         class="rounded-full ml-2 mr-2 mw-50px" alt="">
+                </a>
+                <div class="flex flex-col justify">
+                    <a href="">
+                        <span>Valeria Novodvorskaya</span>
+                    </a>
+                    <span class="text-slate-400 ml-auto">day ago</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex mt-7">
+            <div class="w-1/2 text-lg pr-6">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi assumenda culpa ducimus ea eaque earum et excepturi fuga, neque nesciunt nihil sequi vel? Autem ducimus eos labore minima nisi perferendis quasi quod quos sapiente voluptatum! Amet beatae commodi facere fugiat illo laudantium totam! Ad, aliquam asperiores aspernatur debitis deleniti eos in laborum magnam nobis quasi saepe sequi sit voluptatibus?
+            </div >
+            <div class="w-1/2">
+                <img src="https://via.placeholder.com/350x350"
+                     class="rounded-md" alt="">
+            </div>
+        </div>
+
+
+    </div>
 </template>
 
 <script>
