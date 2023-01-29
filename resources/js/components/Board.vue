54f14af8fab453226b577269bc620295c1a9b760
Александр Агакин <agakinalexandr@gmail.com>
2023-01-29 22:20:55 +0100

    keeping items inside their own component

--- resources/js/components/Board.vue
+++ resources/js/components/Board.vue
@@ -3,95 +3,21 @@
         <input type="text" placeholder="search" class="w-full rounded-xl p-3 focus:outline-none">
     </form>
     <div class="container flex flex-row flex-wrap justify-between mt-7 ">
-        <div class="w-1/2 mb-3">
-            <div class="flex justify-center">
-                <div>
-                    <div class="mb-4 flex">
-                        <a href="">
-                            <img src="https://via.placeholder.com/50x50" class="rounded-full ml-2 mr-2 mw-50px" alt="">
-                        </a>
-                        <div class="flex flex-col ">
-                            <a href="">
-                                <span>Valeria Novodvorskaya</span>
-                            </a>
-                            <span class="text-slate-400">day ago</span>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <img src="https://via.placeholder.com/350x250" class="rounded-md" alt="">
-                    </a>
-                    <div class="text-xl">Product</div>
-                </div>
-            </div>
-        </div>
-        <div class="w-1/2 mb-3">
-            <div class="flex justify-center">
-                <div>
-                    <div class="mb-4 flex">
-                        <a href="">
-                            <img src="https://via.placeholder.com/50x50" class="rounded-full ml-2 mr-2 mw-50px" alt="">
-                        </a>
-                        <div class="flex flex-col ">
-                            <a href="">
-                                <span>Valeria Novodvorskaya</span>
-                            </a>
-                            <span class="text-slate-400">day ago</span>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <img src="https://via.placeholder.com/350x250" class="rounded-md" alt="">
-                    </a>
-                    <div class="text-xl">Product</div>
-                </div>
-            </div>
-        </div>
-        <div class="w-1/2 mb-3">
-            <div class="flex justify-center">
-                <div>
-                    <div class="mb-4 flex">
-                        <a href="">
-                            <img src="https://via.placeholder.com/50x50" class="rounded-full ml-2 mr-2 mw-50px" alt="">
-                        </a>
-                        <div class="flex flex-col ">
-                            <a href="">
-                                <span>Valeria Novodvorskaya</span>
-                            </a>
-                            <span class="text-slate-400">day ago</span>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <img src="https://via.placeholder.com/350x250" class="rounded-md" alt="">
-                    </a>
-                    <div class="text-xl">Product</div>
-                </div>
-            </div>
-        </div>
-        <div class="w-1/2 mb-3">
-            <div class="flex justify-center">
-                <div>
-                    <div class="mb-4 flex">
-                        <a href="">
-                            <img src="https://via.placeholder.com/50x50" class="rounded-full ml-2 mr-2 mw-50px" alt="">
-                        </a>
-                        <div class="flex flex-col ">
-                            <a href="">
-                                <span>Valeria Novodvorskaya</span>
-                            </a>
-                            <span class="text-slate-400">day ago</span>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <img src="https://via.placeholder.com/350x250" class="rounded-md" alt="">
-                    </a>
-                    <div class="text-xl">Product</div>
-                </div>
-            </div>
-        </div>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
     </div>
 </template>
 
 <script>
+import Item from "./Item.vue";
 export default {
-    name: "Board"
+    name: "Board",
+    components: {Item}
 }
 </script>
---
+++ resources/js/components/Item.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-1/2 mb-3">
+        <div class="flex justify-center">
+            <div>
+                <div class="mb-4 flex">
+                    <a href="">
+                        <img src="https://via.placeholder.com/50x50" class="rounded-full ml-2 mr-2 mw-50px" alt="">
+                    </a>
+                    <div class="flex flex-col ">
+                        <a href="">
+                            <span>Valeria Novodvorskaya</span>
+                        </a>
+                        <span class="text-slate-400">day ago</span>
+                    </div>
+                </div>
+                <a href="#">
+                    <img src="https://via.placeholder.com/350x250" class="rounded-md" alt="">
+                </a>
+                <div class="text-xl">Product</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Item",
+
+}
+</script>
